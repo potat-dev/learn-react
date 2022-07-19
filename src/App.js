c3db28565b311bd53deff4f056066cd72245eca7
Den <denis100703@gmail.com>
2022-07-20 00:02:53 +0300

    Add 'Created by' footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import TodoList from './components/TodoList';
 import NewTaskForm from './components/TaskForm';
-// import Alert from 'react-bootstrap/Alert';
-
 import { v4 as uuidv4 } from 'uuid';
 
 const LOCAL_STORAGE_KEY = 'todoApp.todos';
@@ -42,20 +40,31 @@ function App() {
   }
 
   var completedTodos = todos.filter(todo => !todo.completed).length
-  // var alertColor = completedTodos ? 'primary' : 'success'
   completedTodos = completedTodos ? completedTodos : 'no'
 
   return (
-    <div className='d-flex justify-content-center'>
-      <div style={{ padding: '16px', maxWidth: '450px' }}>
-        <h1 className='text-center' style={{ textAligin: 'center' }}>Simple Task List</h1>
-        <h6 className='text-center text-muted mb-3' style={{ textAligin: 'center' }}>
-          You have {completedTodos} tasks to do
-        </h6>
-        {/* <Alert variant={alertColor}>You have {completedTodos} tasks to do</Alert> */}
-        <NewTaskForm onAdd={addHandler} onDelete={clearHandler} label_ref={todoNameRef} />
-        <TodoList todos={todos} toggleTodo={toggleTodo} />
+    <div className="d-flex flex-column min-vh-100" style={{ width: '100%', height: '100%' }}>
+      <div className='d-flex justify-content-center'>
+        <div style={{ padding: '16px', maxWidth: '450px' }}>
+          <h1 className='text-center' style={{ textAligin: 'center' }}>Simple Task List</h1>
+          <h6 className='text-center text-muted mb-3' style={{ textAligin: 'center' }}>
+            You have {completedTodos} tasks to do
+          </h6>
+          <NewTaskForm onAdd={addHandler} onDelete={clearHandler} label_ref={todoNameRef} />
+          <TodoList todos={todos} toggleTodo={toggleTodo} />
+        </div>
       </div>
+
+      <footer className="mt-auto footer">
+        <div className="container" style={{ padding: '0px 16px 16px' }}>
+          <div className="muted text-center text-muted">
+            Simple Task List • Created with ♡ by {' '}
+            <a href="https://github.com/CyberPotat42" className="text-decoration-none" title="Check out my GitHub!">
+              Cyber Potato
+            </a>
+          </div>
+        </div>
+      </footer>
     </div>
   )
 }
