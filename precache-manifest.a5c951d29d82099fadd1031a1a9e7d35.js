579e9198c096c9b4d60ab9b98b32fda09b9e610b
Den <denis100703@gmail.com>
2022-07-20 00:04:04 +0300

    Updates

---
+++ precache-manifest.a5c951d29d82099fadd1031a1a9e7d35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59ae5b07f8748a44fe81",
+    "url": "/learn-react/static/js/main.6d397d74.chunk.js"
+  },
+  {
+    "revision": "1f5a4aa29338276e39d8",
+    "url": "/learn-react/static/js/runtime~main.1bcabeff.js"
+  },
+  {
+    "revision": "22667b667a37f4af9f73",
+    "url": "/learn-react/static/css/2.57024993.chunk.css"
+  },
+  {
+    "revision": "22667b667a37f4af9f73",
+    "url": "/learn-react/static/js/2.419767e3.chunk.js"
+  },
+  {
+    "revision": "2a3871a491453b3e338c26fba64e27ca",
+    "url": "/learn-react/index.html"
+  }
+];
